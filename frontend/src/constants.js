08b34b902951c6635ca5ab52bdf564fd9a7d0f66
VennaKeerthi <145180953+VennaKeerthi@users.noreply.github.com>
2024-12-19 17:01:02 +0530

    Update constants.js

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://mern-ecommerce-app-ybe3.onrender.com';
+export const BASE_URL = 'https://mern-ecommerce-app-kk0y.onrender.com';
 export const PRODUCTS_URL = `${BASE_URL}/api/products`;
 export const USERS_URL = `${BASE_URL}/api/users`;
 export const ORDERS_URL = `${BASE_URL}/api/orders`;
