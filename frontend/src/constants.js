b043eb8aef6b4c79fc58d376a8a60c0a8b6aeb33
VennaKeerthi <145180953+VennaKeerthi@users.noreply.github.com>
2024-12-19 16:10:25 +0530

    Update constants.js

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,7 +1,9 @@
-// export const BASE_URL =
-//   process.env.NODE_ENV === 'develeopment' ? 'http://localhost:5000' : '';
-export const BASE_URL = ''; // If using proxy
-export const PRODUCTS_URL = '/api/products';
-export const USERS_URL = '/api/users';
-export const ORDERS_URL = '/api/orders';
-export const PAYPAL_URL = '/api/config/paypal';
+export const BASE_URL =
+  process.env.NODE_ENV === 'development'
+    ? '' // Uses proxy during development
+    : 'https://mern-ecommerce-app-ybe3.onrender.com'; // Backend URL in production
+
+export const PRODUCTS_URL = `${BASE_URL}/api/products`;
+export const USERS_URL = `${BASE_URL}/api/users`;
+export const ORDERS_URL = `${BASE_URL}/api/orders`;
+export const PAYPAL_URL = `${BASE_URL}/api/config/paypal`;
