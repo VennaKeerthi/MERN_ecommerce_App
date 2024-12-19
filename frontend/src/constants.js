b4f4a1bc98dd0e96187c191658940ae8bc0aaa8b
VennaKeerthi <145180953+VennaKeerthi@users.noreply.github.com>
2024-12-19 16:39:01 +0530

    Update constants.js

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,8 +1,4 @@
-export const BASE_URL =
-  process.env.NODE_ENV === 'development'
-    ? '' // Uses proxy during development
-    : 'https://mern-ecommerce-app-ybe3.onrender.com'; // Backend URL in production
-
+export const BASE_URL = 'https://mern-ecommerce-app-ybe3.onrender.com';
 export const PRODUCTS_URL = `${BASE_URL}/api/products`;
 export const USERS_URL = `${BASE_URL}/api/users`;
 export const ORDERS_URL = `${BASE_URL}/api/orders`;
